fb2afdfa8b9fc99cb3acebe0564f54a07182eb27
dokhung <ehhiop123@naver.com>
2026-09-09 23:01:15 +0900

    header

--- src/page/auth/view/LoginView.tsx
+++ src/page/auth/view/LoginView.tsx
@@ -3,9 +3,20 @@ import React from "react";
 export const LoginView:React.FC = () => {
     return (
         <section className={"min-h-screen flex justify-center items-center"}>
-            <div className={"border w-full max-w-md"}>
+            <div className={"w-full max-w-md"}>
+                {/* UI */}
+                <div>
+                    <header className={"flex justify-center border"}>
+                        <h1>Title</h1>
+                    </header>
+                </div>
                 <div className={"border"}>
-
+                    {/* ID */}
+                    <div>
+                        <label></label>
+                    </div>
+                    {/* PW */}
+                    <div>PW</div>
                 </div>
             </div>
         </section>
